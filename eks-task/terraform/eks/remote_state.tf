b4f19f3a4a0051a4c3f112811668d8a3f0794ce1
mahmoud ibrahem <110599023+mahmoudibrahem10@users.noreply.github.com>
2023-09-13 15:54:44 +0300

    Delete eks-task/terraform/eks directory

--- eks-task/terraform/eks/remote_state.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "eks-with-terraform-bucket"
-    key    = "state/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
--- eks-task/terraform/eks/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  default     = "eu-central-1"
-}
-
-variable "cluster_name" {
-  type        = string
-  default     = "my-eks"
-}
